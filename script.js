11f027210b053c0464cd61bbd996632f28f66f62
roniba23-meet <110028863+roniba23-meet@users.noreply.github.com>
2022-07-26 14:06:08 +0300

    Finished

---
+++ script.js
@@ -0,0 +1,19 @@
+function Starting(){
+  window.location.replace("first_q.html")
+}
+function First_q_yes(){
+  window.location.replace("final.html")
+}
+function First_q_no(){
+  window.location.replace("whatIs.html")
+}
+function Back(){
+  window.location.replace("index.html")
+}
+function Wiki(){
+window.location.replace("https://en.wikipedia.org/wiki/Orange_(fruit)")
+}
+
+function Bg(){
+  document.body.style.backgroundColor = "orange";
+}
